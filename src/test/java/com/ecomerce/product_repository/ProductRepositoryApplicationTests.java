2167f176bca44c5f4036c15ed59f3451d742c2a0
Ayush Verma <=>
2025-02-09 17:14:05 +0530

    Project Repository Initially Created

---
+++ src/test/java/com/ecomerce/product_repository/ProductRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.product_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
