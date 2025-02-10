9613f02e0f3cbe7cd13bd5b23faad31353a84839
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-02-10 15:12:47 +0530

    Merge pull request #2
    * Created Model and implemented restful services
    
    * product Repo Controller Created
    
    * Created Model and implemented restful services
    
    * Merge remote-tracking branch 'origin/Feature_1' into Feature_1

---
+++ src/main/java/com/ecomerce/product_repository/Modells/Category.java
@@ -0,0 +1,11 @@
+package com.ecomerce.product_repository.Modells;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private Integer id;
+    private String title;
+}
