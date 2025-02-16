32f357aa37564cab261d3a7e461f31537cf75311
Ayush Verma <=>
2025-02-17 00:26:43 +0530

    Adding database connection with jpa hibernate

--- src/main/java/com/ecomerce/product_repository/Modells/Products.java
+++ src/main/java/com/ecomerce/product_repository/Modells/Products.java
@@ -1,18 +1,59 @@
 package com.ecomerce.product_repository.Modells;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 
-@Getter
-@Setter
+import java.util.Date;
+
 @Entity
 public class Products extends BaseModel{
-    @Column(name = "title", nullable = false) // Ensure this is mapped correctly
     private String title;
     private String description;
     private String imageUrl;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Category category;
+
+    public Products(int id, Date created_at, Date updated_at, boolean is_deleted) {
+        super();
+    }
+
+    public Products() {
+        super();
+    }
+
+
+    public String getTitle(String title) {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
