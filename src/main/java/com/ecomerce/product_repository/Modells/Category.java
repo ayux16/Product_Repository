1cc9caedcf6c358be5c0d0a970c547e4b0fa3dac
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-02-17 21:21:20 +0530

    Merge pull request #5
    * Adding database connection with jpa hibernate
    
    * DV Setup

--- src/main/java/com/ecomerce/product_repository/Modells/Category.java
+++ src/main/java/com/ecomerce/product_repository/Modells/Category.java
@@ -1,23 +1,19 @@
 package com.ecomerce.product_repository.Modells;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.util.List;
-
-@Getter
-@Setter
 @Entity
 public class Category extends BaseModel {
 
     private String title;
-    @OneToMany(mappedBy = "category",cascade = CascadeType.REMOVE)
-    @JsonIgnore
-    private List<Products> products;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
 }
