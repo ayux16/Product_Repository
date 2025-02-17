bf3512113036b49172ec90e2ffd1ddf4223785db
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-02-17 21:21:20 +0530

    Merge pull request #5
    * Adding database connection with jpa hibernate
    
    * DV Setup

--- src/main/java/com/ecomerce/product_repository/Modells/BaseModel.java
+++ src/main/java/com/ecomerce/product_repository/Modells/BaseModel.java
@@ -4,15 +4,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Date;
 
-@AllArgsConstructor
-@Getter
-@Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
@@ -23,12 +17,43 @@ public class BaseModel {
     private Date updated_at;
     private boolean is_deleted;
 
-    private String created_by_UserName;
 
     public BaseModel() {
         this.id = id;
         this.created_at = created_at;
         this.updated_at = updated_at;
+        this.is_deleted = is_deleted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public boolean isIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(boolean is_deleted) {
         this.is_deleted = is_deleted;
     }
 }
