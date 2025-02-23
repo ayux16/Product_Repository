58d507fbfea3fe24b01095556fcbdff048a34eb6
Ayush Verma <=>
2025-02-23 19:22:40 +0530

    Implemented Database Migration flywaydb

--- src/main/java/com/ecomerce/product_repository/Modells/Category.java
+++ src/main/java/com/ecomerce/product_repository/Modells/Category.java
@@ -5,32 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Category extends BaseModel {
 
     private String title;
-
     @OneToMany(mappedBy = "category",cascade = CascadeType.REMOVE)
     @JsonIgnore
     private List<Products> products;
-
-    public List<Products> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Products> products) {
-        this.products = products;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
 }
