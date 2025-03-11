8af5e28c246b8bd51d04529153ac6956d45fdf41
Ayush Verma <43598530+ayux16@users.noreply.github.com>
2025-03-11 15:59:08 +0530

    Merge pull request #9
    * Self services
    
    * Implemented Database Migration flywaydb
    
    * Merge pull request #5
    
    * war file created and removed additonal sections from pom
    
    * Merge pull request #7
    
    * Self services
    
    * Implemented Database Migration flywaydb
    
    * Merge pull request #7
    
    * Merge pull request #7
    
    * Adding database connection with jpa hibernate
    
    * DV Setup
    
    * Self services
    
    * Implemented Database Migration flywaydb
    
    * Merge pull request #5
    
    * Merge remote-tracking branch 'origin/master' into Feature_1
    
    * war file created and removed additonal sections from pom
    
    * Implemented Database Migration flywaydb
    
    * Merge pull request #7
    
    * Self services
    
    * Implemented Database Migration flywaydb
    
    * Merge pull request #7
    
    * Merge pull request #7
    
    * Merge remote-tracking branch 'origin/Feature_1' into Feature_1

--- src/main/java/com/ecomerce/product_repository/Modells/Category.java
+++ src/main/java/com/ecomerce/product_repository/Modells/Category.java
@@ -1,19 +1,23 @@
 package com.ecomerce.product_repository.Modells;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 public class Category extends BaseModel {
 
     private String title;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    @OneToMany(mappedBy = "category",cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Products> products;
 
 }
--- src/main/java/com/ecomerce/product_repository/Modells/Products.java
+++ src/main/java/com/ecomerce/product_repository/Modells/Products.java
@@ -1,59 +1,18 @@
 package com.ecomerce.product_repository.Modells;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.Date;
-
+@Getter
+@Setter
 @Entity
 public class Products extends BaseModel{
+    @Column(name = "title", nullable = false) // Ensure this is mapped correctly
     private String title;
     private String description;
     private String imageUrl;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.PERSIST)
     private Category category;
-
-    public Products(int id, Date created_at, Date updated_at, boolean is_deleted) {
-        super();
-    }
-
-    public Products() {
-        super();
-    }
-
-
-    public String getTitle(String title) {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 }
