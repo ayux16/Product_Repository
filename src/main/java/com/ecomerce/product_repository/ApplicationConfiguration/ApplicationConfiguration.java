5c04fc447520e099fdedd703b2dc660a32d1f278
Ayush <ayushx16@gmail.com>
2026-05-22 18:17:32 +0530

    AuthUtils,and userDTO added for verifying the token Enabling service discovery

--- src/main/java/com/ecomerce/product_repository/ApplicationConfiguration/ApplicationConfiguration.java
+++ src/main/java/com/ecomerce/product_repository/ApplicationConfiguration/ApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package com.ecomerce.product_repository.ApplicationConfiguration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationConfiguration {
     @Bean
-    public RestTemplate createRestTemplate() {
+    @LoadBalanced
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
